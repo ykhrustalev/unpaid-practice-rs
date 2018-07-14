1b983bdd5b8babf261ca367b88b15c2d905986df
Yuri Khrustalev <yuri.khrustalev@gmail.com>
2018-07-15 00:34:46 +0300

    module

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod queue;
+
 fn main() {
 //    let mut q: Queue<i32> = Queue::new();
 
@@ -6,126 +8,8 @@ fn main() {
 //    q.insert(8);
 //    q.insert(7);
 
-    let q: Queue<i32> = Queue::new_with_elements(&[5, 2, 8, 7]);
-    for i in &q.q {
+    let q: queue::Queue<i32> = queue::Queue::new_with_elements(&[5, 2, 8, 7]);
+    for i in &q.items() {
         println!("{}", i)
     }
-}
-
-
-enum Element {
-    Root,
-    Item(usize),
-}
-
-use Element::*;
-
-impl std::fmt::Debug for Element {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match self {
-            &Root => write!(f, "root"),
-            &Item(n) => write!(f, "item({})", n)
-        }
-    }
-}
-
-impl std::cmp::PartialEq for Element {
-    fn eq(&self, other: &Element) -> bool {
-        match other {
-            &Root => match self {
-                &Root => true,
-                _ => false,
-            },
-            &Item(a) => match self {
-                &Item(b) => a == b,
-                _ => false,
-            },
-        }
-    }
-}
-
-
-fn pg_parent(n: usize) -> Element {
-    match n {
-        0 => Root,
-        x @ _ => Item((x - 1) / 2),
-    }
-}
-
-#[test]
-fn test_pg_parent() {
-    assert_eq!(pg_parent(0), Root);
-    assert_eq!(pg_parent(1), Item(0));
-    assert_eq!(pg_parent(2), Item(0));
-    assert_eq!(pg_parent(9), Item(4));
-    assert_eq!(pg_parent(10), Item(4));
-}
-
-//#[derive(Copy, Clone)]
-pub struct Queue<T> {
-    q: std::vec::Vec<T>,
-    n: usize,
-}
-
-
-impl<T: std::cmp::Ord + std::fmt::Display + std::clone::Clone> Queue<T> {
-    pub fn new() -> Queue<T> {
-        Queue {
-            q: std::vec::Vec::new(),
-            n: 0,
-        }
-    }
-
-    pub fn new_with_elements(items: &[T]) -> Queue<T> {
-        let mut q: Queue<T> = Queue {
-            q: std::vec::Vec::new(),
-            n: 0,
-        };
-
-        for i in items {
-            q.insert(i.clone());
-        }
-        q
-    }
-
-    pub fn insert(&mut self, value: T) {
-        self.q.insert(self.n, value);
-        let n = self.n;
-        self.bubble_up(n);
-        self.n += 1;
-    }
-
-    fn swap(&mut self, a: usize, b: usize) {
-        self.q.swap(a, b)
-    }
-
-    fn bubble_up(&mut self, p: usize) {
-        match pg_parent(p) {
-            Root => {}
-            Item(parent) => {
-                match self.q[parent].cmp(&self.q[p]) {
-                    std::cmp::Ordering::Greater => {
-                        self.swap(p, parent);
-                        self.bubble_up(parent)
-                    }
-                    _ => {}
-                }
-            }
-        }
-    }
-}
-
-#[test]
-fn test_pg() {
-    let mut q: Queue<i32> = Queue::new();
-
-    q.insert(5);
-    q.insert(2);
-    q.insert(8);
-    q.insert(7);
-
-    assert_eq!(q.q[0], 2);
-    assert_eq!(q.q[1], 5);
-    assert_eq!(q.q[2], 8);
-    assert_eq!(q.q[3], 7);
 }
---
+++ src/queue.rs
@@ -0,0 +1,120 @@
+use std;
+use self::Element::*;
+
+enum Element {
+    Root,
+    Item(usize),
+}
+
+impl std::fmt::Debug for Element {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            &Root => write!(f, "root"),
+            &Item(n) => write!(f, "item({})", n)
+        }
+    }
+}
+
+impl std::cmp::PartialEq for Element {
+    fn eq(&self, other: &Element) -> bool {
+        match other {
+            &Root => match self {
+                &Root => true,
+                _ => false,
+            },
+            &Item(a) => match self {
+                &Item(b) => a == b,
+                _ => false,
+            },
+        }
+    }
+}
+
+
+fn pg_parent(n: usize) -> Element {
+    match n {
+        0 => Root,
+        x @ _ => Item((x - 1) / 2),
+    }
+}
+
+#[test]
+fn test_pg_parent() {
+    assert_eq!(pg_parent(0), Root);
+    assert_eq!(pg_parent(1), Item(0));
+    assert_eq!(pg_parent(2), Item(0));
+    assert_eq!(pg_parent(9), Item(4));
+    assert_eq!(pg_parent(10), Item(4));
+}
+
+
+pub struct Queue<T> {
+    q: std::vec::Vec<T>,
+    n: usize,
+}
+
+impl<T: std::cmp::Ord + std::fmt::Display + std::clone::Clone> Queue<T> {
+    pub fn new() -> Queue<T> {
+        Queue {
+            q: std::vec::Vec::new(),
+            n: 0,
+        }
+    }
+
+    pub fn new_with_elements(items: &[T]) -> Queue<T> {
+        let mut q: Queue<T> = Queue {
+            q: std::vec::Vec::new(),
+            n: 0,
+        };
+
+        for i in items {
+            q.insert(i.clone());
+        }
+        q
+    }
+
+    pub fn insert(&mut self, value: T) {
+        self.q.insert(self.n, value);
+        let n = self.n;
+        self.bubble_up(n);
+        self.n += 1;
+    }
+
+    pub fn items(&self) -> std::vec::Vec<T> {
+        self.q.clone()
+    }
+
+    fn swap(&mut self, a: usize, b: usize) {
+        self.q.swap(a, b)
+    }
+
+    fn bubble_up(&mut self, p: usize) {
+        match pg_parent(p) {
+            Root => {}
+            Item(parent) => {
+                match self.q[parent].cmp(&self.q[p]) {
+                    std::cmp::Ordering::Greater => {
+                        self.swap(p, parent);
+                        self.bubble_up(parent)
+                    }
+                    _ => {}
+                }
+            }
+        }
+    }
+}
+
+#[test]
+fn test_queue() {
+    let mut q: Queue<i32> = Queue::new();
+
+    q.insert(5);
+    q.insert(2);
+    q.insert(8);
+    q.insert(7);
+
+    assert_eq!(q.q[0], 2);
+    assert_eq!(q.q[1], 5);
+    assert_eq!(q.q[2], 8);
+    assert_eq!(q.q[3], 7);
+}
